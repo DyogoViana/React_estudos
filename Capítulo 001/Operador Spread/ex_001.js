385e71a23479c9e0ffdd6faeba6e90e4cbdd1041
DyogoViana <dyogo.vianaa@gmail.com>
2021-06-15 17:38:57 -0300

    [ AJUSTE || Capítulo 01 ] : Operador Spread | Exemplo 001.

--- Capítulo 001/Operador Spread/ex_001.js
+++ Capítulo 001/Operador Spread/ex_001.js
@@ -6,17 +6,19 @@ const precos = [70, 30, 90, 100, 10];
 
 
 let livros = livros001 + livros002;
-console.log(livros);
+console.log("Livros " + livros);
 console.log(typeof livros);
 
-let livrosA = [livros001, livros002]
+let livrosA = [livros001, livros002];
+console.log("Livros A abaixo");
 console.log(livrosA);
 
-let livrosB = [...livros001, ...livros002]
+let livrosB = [...livros001, ...livros002];
+console.log("Livros B abaixo");
 console.log(livrosB);
 
 let precoMaximo = Math.max(precos);
 let precoMaximo001 = Math.max(...precos);
-console.log(precoMaximo);
-console.log(precoMaximo001);
+console.log("Preço Máximo " + precoMaximo);
+console.log("Preço Máximo001 " +precoMaximo001);
 
