b77116915c6ab009f1c4a271fe8525d3ef7d15b1
DyogoViana <dyogo.vianaa@gmail.com>
2021-07-01 15:45:25 -0300

    [ Capítulo 01 ] : Método Find.

---
+++ Capítulo 001/002_Métodos map, filter e find/Método Find/ex_001.js
@@ -0,0 +1,25 @@
+// Capítulo 001 | Método Find | Exemplo 001
+
+/*
+	Sintaxe:
+	https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/find
+
+
+	arr.find(callback(element[, index[, array]])[, thisArg])][]
+
+*/
+
+
+const livrosLista =
+	[
+		{id: 1, titulo: "Construindo Sites com HTML"},
+		{id: 2, titulo: "Web Scraping com Python"},
+		{id: 3, titulo: "CSS3"}
+	];
+
+	let livros1 = livrosLista.find((livro) => livro.id === 3);
+
+	console.log(livros1); // {id: 3, titulo: "CSS3"}
+	console.log(livros1.titulo); // CSS3
+
+
